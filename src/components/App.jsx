6b7a1581286458eee69f9f71a656753b52be8aac
hevedrios <exxed.ridel@gmail.com>
2022-10-05 15:52:38 -0500

    React 18.2 initializated

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/global.scss";
+
+const App = () => {
+  return (
+    <div>
+      <h1>This is gonna be the Speed Typing Game</h1>
+    </div>
+  );
+};
+
+export default App;
