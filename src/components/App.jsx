e1e260ae0ac200552cc41dbdb2c217b5634b0d01
hevedrios <exxed.ridel@gmail.com>
2022-10-05 21:38:02 -0500

    Added: textarea state and count words within the string

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/global.scss";
 
 const App = () => {
+  const [textArea, setTextArea] = useState("");
+  console.log(textArea);
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setTextArea(value);
+  }
+
+  function countWords(textArea) {
+    // const words = textArea.trim().split(" ");
+    const words = textArea.split(" ");
+    return console.log(words.filter((word) => word !== "").length); //with this aproach .trim() no longer needed
+  }
+
   return (
     <div>
       <h1>How fast do you type?</h1>
-      <textarea />
+      <textarea value={textArea} placeholder="" onChange={handleChange} />
       <h4>Time reminaing: ???</h4>
-      <button>Start</button>
+      <button onClick={() => countWords(textArea)}>Start</button>
       <h1>Word count: ???</h1>
     </div>
   );
