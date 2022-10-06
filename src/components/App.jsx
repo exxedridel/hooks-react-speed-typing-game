c150eaa037646df2e86b39c1be066db52047f59e
hevedrios <exxed.ridel@gmail.com>
2022-10-05 19:38:31 -0500

    Added: CSS and HTML

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,11 @@ import "../styles/global.scss";
 const App = () => {
   return (
     <div>
-      <h1>This is gonna be the Speed Typing Game</h1>
+      <h1>How fast do you type?</h1>
+      <textarea />
+      <h4>Time reminaing: ???</h4>
+      <button>Start</button>
+      <h1>Word count: ???</h1>
     </div>
   );
 };
